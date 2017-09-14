37de638d3dfc5395d1860f78132422a5899f5929
Xander Deng <dengxiang2010@gmail.com>
2017-09-14 21:27:43 +0800

    extract lyrics merging to a new file

---
+++ Sources/Provider/Utilities/Lyrics+Merge.swift
@@ -0,0 +1,44 @@
+//
+//  Lyrics+Merge.swift
+//
+//  This file is part of LyricsX
+//  Copyright (C) 2017  Xander Deng
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+
+extension Lyrics {
+    
+    func merge(translation: Lyrics) {
+        var index = lines.startIndex
+        var transIndex = translation.lines.startIndex
+        while index < lines.endIndex, transIndex < translation.lines.endIndex {
+            if lines[index].position == translation.lines[transIndex].position {
+                let transStr = translation.lines[transIndex].content
+                if !transStr.isEmpty {
+                    let translation = LyricsLineAttachmentPlainText(transStr)
+                    lines[index].attachments[.translation] = translation
+                }
+                lines.formIndex(after: &index)
+                translation.lines.formIndex(after: &transIndex)
+            } else if lines[index].position > translation.lines[transIndex].position {
+                translation.lines.formIndex(after: &transIndex)
+            } else {
+                lines.formIndex(after: &index)
+            }
+        }
+    }
+}
