369f60b67a5ccc38f309c79525770e5bac414981
Xander Deng <dengxiang2010@gmail.com>
2020-02-16 23:23:29 +0800

    fix: deployment target when using combine

--- Package.swift
+++ Package.swift
@@ -36,3 +36,39 @@ let package = Package(
         .v5
     ]
 )
+
+enum CombineImplementation {
+    
+    case combine
+    case combineX
+    case openCombine
+    
+    static var `default`: CombineImplementation {
+        #if canImport(Combine)
+        return .combine
+        #else
+        return .combineX
+        #endif
+    }
+    
+    init?(_ description: String) {
+        let desc = description.lowercased().filter { $0.isLetter }
+        switch desc {
+        case "combine":     self = .combine
+        case "combinex":    self = .combineX
+        case "opencombine": self = .openCombine
+        default:            return nil
+        }
+    }
+}
+
+import Foundation
+
+let env = ProcessInfo.processInfo.environment
+let impkey = "CX_COMBINE_IMPLEMENTATION"
+
+var combineImp = env[impkey].flatMap(CombineImplementation.init) ?? .default
+
+if combineImp == .combine {
+    package.platforms = [.macOS("10.15"), .iOS("13.0"), .tvOS("13.0"), .watchOS("6.0")]
+}
